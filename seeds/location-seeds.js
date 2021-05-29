28b1a65bfc0e1d2cb2cc2c36bbe2200a4c6707e0
[Andrew Core] <[adcore89@gmail.com]>
2021-05-29 15:30:22 -0400

    location seed started

---
+++ seeds/location-seeds.js
@@ -0,0 +1,14 @@
+//const sequelize = require('../config/connection');
+const { location } = require('../models')
+
+//id, Restaurant number, restaurant addresss 
+
+const locationdata =[
+
+
+
+]
+
+const seedLocation = () => Comment.bulkCreate(locationdata);
+
+module.exports = seedLocation;
