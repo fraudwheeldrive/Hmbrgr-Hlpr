635c7c816eb85899cd60c1e6a9c02b642370b75e
Swan Pham <accounting@nailsrus.ca>
2021-05-28 23:27:08 -0400

    link location, move index.html to main.handlebars.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require( 'express' ).Router();
+router.get( '/', (req, res ) => {
+    res.render( 'homepage' )
+} );
+router.get( '/contact', (req, res ) => {
+    res.render( 'contact' )
+} );
+router.get( '/location', (req, res ) => {
+    res.render( 'locations' )
+} );
+router.get( '/menu', (req, res ) => {
+    res.render( 'menu' )
+} );
+router.get( '/login', (req, res ) => {
+    res.render( 'login' )
+} );
+module.exports = router;
