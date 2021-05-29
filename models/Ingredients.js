09e27af7c1c9b5dbeb2d931c906afbf228ad52e2
[Andrew Core] <[adcore89@gmail.com]>
2021-05-29 11:55:53 -0400

    pseudo code models

---
+++ models/Ingredients.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequlize');
+const sequlize = require('../config/connection');
+
+
+// ID
+// Restaurant Number
+// Restaurant Address
+
+
+class Ingredients extends Model {
+
+
+
+
+    Ingredients.init(
+        {}
+    )
+}
+
+module.exports = ingredients;
