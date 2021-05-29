6b7b5862e271b719b1ae2849f975de4d13552e61
Swan Pham <accounting@nailsrus.ca>
2021-05-28 22:02:59 -0400

    add index.html to main.handlebars

--- models/User.js
+++ models/User.js
@@ -6,16 +6,39 @@ class User extends Model {}
 
 
 User.init(
-    {
-      
-    },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,      
+      allowNull: false,      
+      primaryKey: true,      
+      autoIncrement: true
+    },    
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },    
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,     
+      unique: true,      
+      validate: {
+        isEmail: true
+      }
+    },    
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {        
+        len: [5]
+      }
     }
-  );
-  
-  module.exports = User;
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
