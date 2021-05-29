a69558c7997151d82889d45ec9cbe8c0b49ef83b
[Andrew Core] <[adcore89@gmail.com]>
2021-05-29 10:14:53 -0400

    user seeds basic logic created

---
+++ seeds/user-seeds.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/connection');
+const {User,} = require('../models');
+
+const userdata = [
+    {
+        Username: 'headhmbrgr',
+        email: 'owner@hmbrgr.com',
+        password: 'password123'
+    },
+    {
+        Username: 'Manager1',
+        email: 'manager@hmbrgr.com',
+        password: 'password123'
+    },
+    {
+        Username: 'TeamLead',
+        email: 'Teamlead@hmbrgr.com',
+        password: 'password123'
+    },
+    {
+        Username: 'SocialMedia',
+        email: 'socialmedia@hmbrgr.com',
+        password: 'password123'
+    }
+];
+
+const seedUser = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUser;
+
+
