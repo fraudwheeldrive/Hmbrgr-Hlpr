684ba6c018445fa669896625aef20cb8d5626a47
Michelle <michey.asmah21@gmail.com>
2021-05-31 22:48:46 -0400

    get restaurant full menu

--- server.js
+++ server.js
@@ -27,19 +27,6 @@ app.engine( 'handlebars', hbs.engine );
 app.set( 'view engine', 'handlebars' );
 app.use( routes );
 
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-// app.use(session(sess));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
