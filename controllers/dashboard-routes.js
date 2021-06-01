26f3f5bcfaaac58aa0e052c678c95293efa971e5
Michelle <michey.asmah21@gmail.com>
2021-06-01 08:13:10 -0400

    dashboard and links

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,15 +3,40 @@ const sequelize = require('../config/connection');
 const { Restaurant, Menu, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get( '/restaurants', (req, res ) => {
-	res.render( 'restaurants' )
-});
-
 router.get('/', withAuth, (req, res) => {
 	res.render('dashboard', {
-			loggedIn: req.session.loggedIn
+			loggedIn: true
 	});
 })
 
+router.get( '/new-location', (req, res ) => {
+	res.render('newlocation', {
+		loggedIn: true
+	});
+});
+
+router.get( '/new-menu', (req, res ) => {
+	res.render('newmenu', {
+		loggedIn: true
+	});
+});
+
+router.get( '/all-menu-items', (req, res ) => {
+	res.render('allmenu', {
+		loggedIn: true
+	});
+});
+
+router.get( '/location-menu', (req, res ) => {
+	res.render('locationmenu', {
+		loggedIn: true
+	});
+});
+
+router.get( '/close-location', (req, res ) => {
+	res.render('closelocation', {
+		loggedIn: true
+	});
+});
 
 module.exports = router;
