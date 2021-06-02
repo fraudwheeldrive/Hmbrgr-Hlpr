17ed733d713abf7d4bea5817c0ab6c5a3e1df8a1
Swan Pham <accounting@nailsrus.ca>
2021-06-02 00:24:13 -0400

    link all locations

--- public/javascript/contact.js
+++ public/javascript/contact.js
@@ -1,34 +1,42 @@
 function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const firstName = document.querySelector('#first-name').value.trim();
-    const lastName = document.querySelector('#last-name').value.trim();
-    const email = document.querySelector('#inputEmail4').value.trim();
-    const address = document.querySelector('#inputAddressl').value.trim();
-    const address2 = document.querySelector('#inputAddressl2').value.trim();
-    const city = document.querySelector('#inputCity').value.trim();
-    const state = document.querySelector('#inputState').value.trim();
-    const zip = document.querySelector('#inputZip').value.trim();
-    const gridCheck = document.querySelector('#gridCheck').value.trim();
-   
-    
-      fetch('/api/contact', {
-        method: 'post',
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          address,
-          address2,
-          city,
-          state,
-          zip,
-          gridCheck          
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      }).then((response) => {console.log(response)})
-    
+  event.preventDefault();
+  // console.log( 'click' )
+  const firstName = document.querySelector('#first-name');
+  const lastName = document.querySelector('#last-name');
+  const email = document.querySelector('#inputEmail4');
+  const address = document.querySelector('#inputAddress');
+  const address2 = document.querySelector('#inputAddress2');
+  const city = document.querySelector('#inputCity');
+  const state = document.querySelector('#inputState');
+  const zip = document.querySelector('#inputZip');
+
+  const formContainer = document.querySelector('.background-contact');
+
+// console.log( firstName , lastName, email , address , address2 , city , state , zip )
+  if( firstName && lastName && email && address && address2 && city && state && zip ) {
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstname: firstName.value,
+        lastname: lastName.value,
+        email: email.value,
+        address: address.value,
+        address2: address2.value,
+        city: city.value,
+        state: state.value,
+        zip: zip.value         
+      }),
+    })
+    .then((response) => response.json())
+    .then(json => console.log( json ))
+    .then(formContainer.innerHTML = '<h1> Successfully Submitted</h1>')
+    .then(window.scrollTo({ top: 0, behavior: 'smooth' }))
+  }
 }
-  
 document.querySelector('.contactForm').addEventListener('submit', signupFormHandler);
+
 
--- seeds/restaurant-seeds.js
+++ seeds/restaurant-seeds.js
@@ -10,30 +10,28 @@ const { Restaurant } = require("../models");
 
 const restaurantdata = [
   {
-    Id: 1,
     restaurant_number: 4,
+    city: "Toronto",
     address: "123 function rd, Toronto Ontario",
     user_id: 1
   },
   {
-    Id: 2,
     restaurant_number: 4,
+    city: "Milton",
     address: "456 method crt, Milton Ontario",
     user_id: 1
-
   },
   {
-    Id: 3,
     restaurant_number: 4,
+    city: "London",
     address: "890 Variable Blvd, London Ontario",
     user_id: 1 
   },
   {
-    Id: 4,
     restaurant_number: 4,
+    city: "Missisauga",
     address: "246 Array Way, Missisauga Ontario",
     user_id: 1
-    
   }
 ];
 
